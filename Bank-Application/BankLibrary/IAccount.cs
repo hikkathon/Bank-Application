ee13bf3fd662b810b96e8bdefc11168d8b95af5c
hikkathon <hikkathon@gmail.com>
2020-07-31 21:02:46 +0700

    +lib

---
+++ Bank-Application/BankLibrary/IAccount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankLibrary
+{
+    public interface IAccount
+    {
+        // Положить деньги на счёт
+        void Put(decimal sum);
+        // Взять со счёта
+        decimal Withdraw(decimal sum);
+    }
+}
