6ce7774029d4fa1de6df4e36c5fdc59a2b7aa563
hikkathon <hikkathon@gmail.com>
2020-08-30 14:31:08 +0700

    Добавил класс DemandAccount
    Класс, который будет представлять счет до востребования.

---
+++ Bank-Application/BankLibrary/DemandAccount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankLibrary
+{
+    public class DemandAccount : Account
+    {
+        public DemandAccount(decimal sum, int percentage) : base(sum, percentage) { }
+
+        protected internal override void Open()
+        {
+            base.OnOpened(new AccountEventArgs($"Открыт новый счёт до востребования! Id счёта: {this.Id}", this.Sum));
+        }
+    }
+}
